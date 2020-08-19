ef9eb1f74313ce074012ad388602dce126c9f094
Lilita1762 <lilibeth.tabares@gmail.com>
2020-08-19 16:15:56 +0000

    Adding all prototypes

--- mini_shell.h
+++ mini_shell.h
@@ -23,6 +23,20 @@ typedef struct path
 } path_st;
 
 /**
+ * struct _free - struct that will have the pointers to free
+ * @f_header_PATH: linked list that will have the PATH
+ * @f_buff_line: string set by get line
+ * @f_myname: the name of this program
+ */
+typedef struct _free
+{
+	path_st **f_header_PATH;
+	char **f_buff_line;
+	char **f_myname;
+	char ***f_arguments;
+} tofree_st;
+
+/**
  * struct built - the struct to the built-ins
  * @name: the name of the built-in
  * @func: the function to the built-in
@@ -30,10 +44,16 @@ typedef struct path
 typedef struct built
 {
 	char *name;
-	int (*func)(char **argv, int c, char *n);
+	int (*func)(char **argv, int c, tofree_st tofree, char *n);
 } built_in;
 
 /* ========== PROTOTYPES ========== */
+/* main.c */
+int execute_execve(path_st *header, char *const argv[]);
+void set_argv(char **argv, char *buff, const char *delim);
+char *save_name(char *src);
+void print_error(int count, char *name, char *command);
+
 /* create_linked.c */
 path_st *create_linkedlist_path(char *path);
 void _new_node(char *s, path_st **head);
@@ -42,13 +62,22 @@ void free_PATH(path_st *head);
 char *_getenv(const char *name);
 
 /* builtin.c */
-int mini_exit(char **argv, int c, char *n);
-int mini_cd(char **argv, int c, char *n);
-int mini_env(char **argv, int c, char *n);
-int find_builtin(char **argv, built_in *built_names, int c, char *n);
+int mini_exit(char **argv, int c, tofree_st tofree, char *n);
+int mini_cd(char **argv, int c, tofree_st tofree, char *n);
+int mini_env(char **argv, int c, tofree_st tofree, char *n);
+int find_builtin(char **argv, built_in *built_names, int c, tofree_st tofree, char *n);
+int _strcmp(char *s1, char *s2);
 
 /* _puts.c */
 int _puts(char *str);
 char *tostring(char *scount, int count);
+
+
+/* THE OTHERS */
+/* init_arguments.c */
+char **init_arguments(void);
+
+/* free_all.c */
+void free_all(tofree_st tofree);
 
 #endif /* MINI_SHELL_H */
